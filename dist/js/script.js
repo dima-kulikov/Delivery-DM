06e3f0cb67dac831d3655256af28fdd9afd9a063
dima-kulikov <skaizy44@gmail.com>
2021-08-10 19:02:01 +0300

    dsds
    sdsd

--- dist/js/script.js
+++ dist/js/script.js
@@ -29,53 +29,49 @@
 
 
 //ВТОРОЙ СПОСОБ НАВЕСИТЬ КЛАСС ПРИ ПРОКРУТКЕ
-
 window.addEventListener('scroll', () => {
 	let scrollDistance = window.scrollY;
 
 	// if (window.innerWidth > 768) {
-		document.querySelectorAll('.section').forEach((el, i) => {
-			if (el.offsetTop - document.querySelector('.page-nav').clientHeight <= scrollDistance) {
-				document.querySelectorAll('.page-nav a').forEach((el) => {
-					if (el.classList.contains('active')) {
-						el.classList.remove('active');
+		document.querySelectorAll('.dl-section').forEach((el, i) => {
+			if (el.offsetTop - document.querySelector('.dl-page-nav').clientHeight <= scrollDistance) {
+				document.querySelectorAll('.dl-page-nav a').forEach((el) => {
+					if (el.classList.contains('dl-active')) {
+						el.classList.remove('dl-active');
 					}
 				});
 
-				document.querySelectorAll('.page-nav li')[i].querySelector('a').classList.add('active');
+				document.querySelectorAll('.dl-page-nav li')[i].querySelector('a').classList.add('dl-active');
 			}
 		});
 	// }
 });
+
 //-----------------------
-// const observer = new IntersectionObserver(onEntry, options);
-// const section = document.querySelectorAll('.section');
-
-// section.forEach(section =>{
-//     observer.observe(section);
-// });
-
-
-const accordeons = document.querySelectorAll('.info-tabs-item__title');
-const replacementIcon = document.querySelectorAll('.section__open-btn');
-
+const accordeons = document.querySelectorAll('.dl-info-tabs-item__title');
+const replacementIcon = document.querySelectorAll('.dl-section__open-btn');
 
 function headerClick () {
-    // el.classList.toggle('open');
     console.log(this);
     console.log(this.parentElement);
-    this.parentElement.classList.toggle('open');
-    this.parentElement.querySelector('.section__open-btn').classList.toggle('section__open-btn__del-after');
+    this.parentElement.classList.toggle('dl-open');
+    this.parentElement.querySelector('.dl-section__open-btn').classList.toggle('dl-section__open-btn__del-after');
     this.preventDefault()
 };
-
 accordeons.forEach(el => {
     el.addEventListener('click', headerClick);
 })
     
-
-console.log(document.querySelector('.delivery-padding').parentElement);
-
-// Добавление к части ссылки страница
-
-window.location.hash="js";
+//------
+let scrollValue = document.getElementsByClassName("dl-grid-block")[0];   
+let bot = document.getElementsByClassName("header-mobile")[0];    
+		
+	 window.addEventListener('scroll', function() {
+	  if (bot.classList.contains("header-mobile--hide")) {
+	  scrollValue.style.top='0px'
+	}
+		else{
+		  scrollValue.style.top= bot.offsetHeight + 'px';
+		}
+	});    	
+//------
